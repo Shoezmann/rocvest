5f22f40dde38996cfd76d36270c2e9a0f0d5e179
Lindelwa Nkosinathi <shoezmojake@gmail.com>
2022-05-07 11:17:30 +0200

    scripts loading fixed

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, AfterContentInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterContentInit(){
+    this.jsfileload("/assets/js/vendor/jquery.min.js");
+    this.jsfileload("/assets/js/vendor/jquery.easing.min.js");
+    this.jsfileload("/assets/js/vendor/jquery.inview.min.js");
+    this.jsfileload("/assets/js/vendor/popper.min.js");
+    this.jsfileload("/assets/js/vendor/bootstrap.min.js");
+    this.jsfileload("/assets/js/vendor/ponyfill.min.js");
+    this.jsfileload("/assets/js/vendor/slider.min.js");
+    this.jsfileload("/assets/js/vendor/animation.min.js");
+    this.jsfileload("/assets/js/vendor/progress-radial.min.js");
+    this.jsfileload("/assets/js/vendor/bricklayer.min.js");
+    this.jsfileload("/assets/js/vendor/gallery.min.js");
+    this.jsfileload("/assets/js/vendor/shuffle.min.js");
+    this.jsfileload("/assets/js/vendor/particles.min.js");
+    this.jsfileload("/assets/js/main.js");
+  }
+  ngOnDestroy(){
+    this.jsfileload("/assets/js/vendor/jquery.min.js");
+    this.jsfileload("/assets/js/vendor/jquery.easing.min.js");
+    this.jsfileload("/assets/js/vendor/jquery.inview.min.js");
+    this.jsfileload("/assets/js/vendor/popper.min.js");
+    this.jsfileload("/assets/js/vendor/bootstrap.min.js");
+    this.jsfileload("/assets/js/vendor/ponyfill.min.js");
+    this.jsfileload("/assets/js/vendor/slider.min.js");
+    this.jsfileload("/assets/js/vendor/animation.min.js");
+    this.jsfileload("/assets/js/vendor/progress-radial.min.js");
+    this.jsfileload("/assets/js/vendor/bricklayer.min.js");
+    this.jsfileload("/assets/js/vendor/gallery.min.js");
+    this.jsfileload("/assets/js/vendor/shuffle.min.js");
+    this.jsfileload("/assets/js/vendor/particles.min.js");
+    this.jsfileload("/assets/js/main.js");
+  }
+
+  jsfileload(url: any){
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node); 
   }
 
 }
